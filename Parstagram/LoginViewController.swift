1eea9117171ef09583948cc6a3ff9f915f2c79df
Manniec <manniec00@gmail.com>
2021-03-19 22:28:38 -0700

    Add outlets and actions from login UI elements to swift file

--- Parstagram/LoginViewController.swift
+++ Parstagram/LoginViewController.swift
@@ -8,14 +8,23 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func onSignUp(_ sender: Any) {
+    }
+    @IBAction func onSignIn(_ sender: Any) {
+    }
+    
+    
     /*
     // MARK: - Navigation
 
