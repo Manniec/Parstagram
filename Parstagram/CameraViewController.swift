c09e97a1affefa5f8c10a58fd39c622d91846a58
Manniec <manniec00@gmail.com>
2021-03-19 23:38:28 -0700

    import parse & temporary set camera view control to primary (for testing)

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import AlamofireImage //for resizeing image
+import Parse
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
