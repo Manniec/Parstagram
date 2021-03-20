abb05b024cf12c7d9310c6b33bb23753d755eb3b
Manniec <manniec00@gmail.com>
2021-03-20 05:42:09 -0700

    pull post author and caption from query

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -6,18 +6,57 @@
 //
 
 import UIKit
+import Parse
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var posts = [PFObject]() //array called posts
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated) //after you compose a post you want view to refresh again
+        
+        //Create a query
+        let query = PFQuery(className:"Posts")
+        query.includeKey("author") //author column stores a pointer, we want it to return actuall object with this line
+        query.limit = 20
+        
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil{ //if posts isnt empty
+                self.posts = posts! //Store ur data, put posts in array
+                self.tableView.reloadData() //refresh feed to display posts
+            }
+        }
+    }
+    
+    //Two required functions for datasource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //grab the cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell //corresponds to postcell swift file, so u can use outlets
+        
+        //get post from query
+        let post = posts[indexPath.row]
+        
+        let user = post["author"] as! PFUser //cast author item as PFUser
+        cell.usernameLabel.text = user.username
+        cell.captionLabel.text = post["caption"] as! String
+        
+        return cell
+    }
 
     /*
     // MARK: - Navigation
